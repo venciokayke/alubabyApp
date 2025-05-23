91661be848d15948848e97835893413274accac6
Kayke Borges Vêncio <venciokayke@gmail.com>
2025-05-22 23:11:06 -0300

    SCRUM-9 Refatorar o fluxo de integração e os ativos; atualizar a configuração e os estilos do PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// filepath: c:\Users\Kayke\OneDrive\Documentos\AluBaby\alubabyApp\postcss.config.js
+import tailwindcss from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
+
+/** @type {import('postcss').ProcessOptions} */
+export default {
+  plugins: [tailwindcss, autoprefixer],
+};
